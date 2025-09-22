49d55234eadb43d5325d052f9aa71289551d2161
mydaisg <my@daisg.com>
2025-09-22 20:05:53 +0800

    Create SpiderAuto.R

---
+++ SpiderAuto.R
@@ -0,0 +1,18 @@
+########### Auto Spider Logic #######
+# 自动执行各赛道的最新更新及辨别逻辑
+## 常量（各赛道开奖周几、开奖时间/
+## 变量（当前周几时间
+## 判断（最后数据至今是否发生开奖活动
+SK_CurrentTime <- Sys.time()
+SK_CurrentDay <- Sys.Date()
+SK_CurrentWeek <- 
+SK_OpenTime <- "22:00"
+SK_TrackNumber <- 4
+SK_WeekNumber <- 7
+
+SK_SpiderAuto <- function(){
+  # 矩阵：赛道1～4，星期1～7，
+  
+  open_venues <- c(1,3,4)
+  return(open_venues)
+}
